fa65be6944b67e28a2e09b50b8fcba2378612152
Luben69 <lpacharazov123@gmail.com>
2025-01-25 12:05:05 +0200

    del

--- Kaggle_Stuff/Intro_To_Programming/Data_Types/Question_4.py
+++
@@ -1,9 +0,0 @@
-from learntools.core import binder
-binder.bind(globals())
-from learntools.intro_to_programming.ex3 import *
-print('Setup complete.')
-print(False + False)
-print(True + False)
-print(False + True)
-print(True + True)
-print(False + True + True + True)
