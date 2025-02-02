7c5d7bb765c9982f6567542b04e9856efe6bc0c2
Luben69 <lpacharazov123@gmail.com>
2025-02-02 18:39:05 +0200

    oops

--- Python_Advanced_September_2024/Error_Handling_Lab/Repeat_Text.py
+++
@@ -1,9 +0,0 @@
-our_string = input("String to repeat:\n")
-
-while True:
-    try:
-        x = int(input())
-        print(our_string * x)
-        break
-    except ValueError:
-        print("Variable times must be an integer")
